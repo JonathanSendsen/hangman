f807af9d64f3f850ffa54da01a07965c07544ae2
Jonathan Sendsen <jonathan@coinsmart.com>
2018-10-07 20:34:37 -0400

    minor id vs class button clean up

--- assets/js/game.js
+++ assets/js/game.js
@@ -159,7 +159,7 @@ document.onkeydown = function(event) {
 
 // This section will allows hints to be displayed based on correctWords array
 
-// Allows users to click hint button and recieve and alert
+// Allows users to click hint button and recieve an alert
 var showHint = document.getElementById("hint").onclick = function () {
 
 showHint = ['Computer Language',
